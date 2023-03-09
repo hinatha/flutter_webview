530b54ee80f75227e6f2998038fcfe845f778039
hinatha <hinatha0110@gmail.com>
2023-03-09 17:38:18 +0900

    Add comment in input_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart'; // contains widgets implementing the Material Design guidelines
 import 'package:flutter_riverpod/flutter_riverpod.dart'; // state management library for Flutter
-import 'package:flutter_webview/view/input_page.dart'; // 'root folder name'/view/input_page.dart
+import 'package:flutter_webview/view/input_page.dart'; // 'name in pubspec.yaml'/view/input_page.dart
 
 // entry point of the application
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-// StatelessWidget can make widgets easily and in material.dart
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   // It overrides the build() method to create the MaterialApp widget
   @override
   Widget build(BuildContext context) {
+    // MaterialApp let us use material design
     return MaterialApp(
       // hides the debug banner in the top-right corner of the screen
       debugShowCheckedModeBanner: false,
-      // the app title in the widget
+      // the app title
       title: 'WebView',
-      // widget color
+      // Set default data in this app
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/view/input_page.dart
+++ lib/view/input_page.dart
@@ -1,14 +1,17 @@
-import 'package:adaptive_dialog/adaptive_dialog.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_webview/provider.dart';
-import 'package:flutter_webview/view/web_page.dart';
+import 'package:adaptive_dialog/adaptive_dialog.dart'; // pre-built dialogs for different platforms
+import 'package:flutter/material.dart'; // contains widgets implementing the Material Design guidelines
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // state management library for Flutter
+import 'package:flutter_webview/provider.dart'; // 'name in pubspec.yaml'/provider.dart
+import 'package:flutter_webview/view/web_page.dart'; // 'name in pubspec.yaml'/view/web_page.dart
 
 class InputPage extends ConsumerWidget {
   const InputPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // manage text field widget
     final _controller = TextEditingController();
+    // ref.watch: listen to changes in the state and rebuild the widget tree.
+    // provider.notifier: access the notifier property of the provider object.
     final _provider = ref.watch(provider.notifier);
     return Scaffold(
       appBar: AppBar(
@@ -23,11 +26,13 @@ class InputPage extends ConsumerWidget {
         ],
       ),
       body: Padding(
+        // Set padding in body
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
             TextField(
               controller: _controller,
+              // change textfield state
               onChanged: (value) {
                 _provider.state = value;
               },
@@ -36,7 +41,9 @@ class InputPage extends ConsumerWidget {
             ElevatedButton(
               onPressed: () {
                 if (_provider.state == "") {
+                  // showOkAlertDialog is in adaptive_dialog package.
                   showOkAlertDialog(
+                    // context provides the context of the current widget
                     context: context,
                     title: 'URLを入力してください',
                   );

f121eaa3da8f88e47686e56c93e8fc3d6cb8ef34
hinatha <hinatha0110@gmail.com>
2023-03-09 16:52:28 +0900

    Add comment in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart'; // contains widgets implementing the Material Design guidelines
+import 'package:flutter_riverpod/flutter_riverpod.dart'; // state management library for Flutter
 import 'package:flutter_webview/view/input_page.dart'; // 'root folder name'/view/input_page.dart
 
+// entry point of the application
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
+// StatelessWidget can make widgets easily and in material.dart
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  // It overrides the build() method to create the MaterialApp widget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // hides the debug banner in the top-right corner of the screen
       debugShowCheckedModeBanner: false,
+      // the app title in the widget
       title: 'WebView',
+      // widget color
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      // top page content
       home: const InputPage(),
     );
   }
